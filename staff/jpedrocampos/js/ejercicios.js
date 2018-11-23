be45ab446f5193f7056854a23b916b3c6bc8c2ef
José Pedro Campos <info@jpcampos.com>
2018-11-23 17:24:12 +0000

    js added

---
+++ staff/jpedrocampos/js/ejercicios.js
@@ -0,0 +1,11 @@
+//larger_integer
+var largerInt = function(numb1, numb2){
+    if(numb1 !== numb2){
+        if(numb1 > numb2){
+            console.log('First is larger.');
+        }
+        else{
+            console.log('Second is larger');
+        }
+    }
+}
