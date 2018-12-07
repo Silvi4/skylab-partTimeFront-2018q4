240c5bb49c135700edf618f8a0d490b886566233
Cristiam Da Silva <cristiam86@gmail.com>
2018-12-07 23:58:43 +0100

    added testing challenge and example

--- stuff/week5/webpack_test/src/index.js
+++ stuff/week5/webpack_test/src/index.js
@@ -1,17 +1,22 @@
 
-var sum = function(a, b){
-  if (isNaN(a) || isNaN(b)) {
-    return 'no sumes peras con manzanas.';
-  }
+// var sum = function(a, b){
+//   if (isNaN(a) || isNaN(b)) {
+//     return 'no sumes peras con manzanas.';
+//   }
 
-  return parseInt(a) + parseInt(b);
+//   return parseInt(a) + parseInt(b);
+// }
+
+// var subs = function(a, b) {
+//   return a - b;
+// }
+
+// exports.sum = sum;
+// exports.subs = subs;
+
+var rockPaperScissor = function(hand1, hand2) {
+
+  return '';
 }
 
-var subs = function(a, b) {
-  return a - b;
-}
-
-exports.sum = sum;
-exports.subs = subs;
-
-
+exports.rockPaperScissor = rockPaperScissor;
--- stuff/week5/webpack_test/test/myFirstTest.spec.js
+++ stuff/week5/webpack_test/test/myFirstTest.spec.js
@@ -1,5 +1,74 @@
-import {sum} from '../src/index.js';
+import {rockPaperScissor} from '../src/index.js';
 
+describe('Test hand1 wins', function() {
+
+  test('scissors wins over paper', function(done) {
+    var result = rockPaperScissors("scissors", "paper");
+    expect(result).toBe("scissors vs paper => scissors wins!")+
+    done();
+  })
+
+  test('rock wins over scissors', function(done) {
+    var result = rockPaperScissors("rock", "scissors");
+    expect(result).toBe("rock vs scissors => rock wins!");
+    done();
+  })
+
+  test('paper wins over rock', function(done) {
+    var result = rockPaperScissors("paper", "rock");
+    expect(result).toBe("paper vs rock => paper wins!");
+    done();
+  })
+  
+})
+
+describe('Test hand2 wins', function() {
+
+  test('scissors wins over paper', function(done) {
+    var result = rockPaperScissors("paper", "scissors");
+    expect(result).toBe("paper vs scissors => scissors wins!")+
+    done();
+  })
+
+  test('rock wins over scissors', function(done) {
+    var result = rockPaperScissors("scissors", "rock");
+    expect(result).toBe("scissors vs rock => rock wins!");
+    done();
+  })
+
+  test('paper wins over rock', function(done) {
+    var result = rockPaperScissors("rock", "paper");
+    expect(result).toBe("rock vs paper => paper wins!");
+    done();
+  })
+  
+})
+
+describe('Test tie', function() {
+
+  test('scissors tie', function(done) {
+    var result = rockPaperScissors("scissors", "scissors");
+    expect(result).toBe("scissors vs scissors => tie!")+
+    done();
+  })
+
+  test('rock tie', function(done) {
+    var result = rockPaperScissors("rock", "rock");
+    expect(result).toBe("rock vs rock => tie!");
+    done();
+  })
+
+  test('paper tie', function(done) {
+    var result = rockPaperScissors("paper", "paper");
+    expect(result).toBe("paper vs paper => tie!");
+    done();
+  })
+  
+})
+
+
+
+/*
 describe('Test my sum function',function(){
   var a, b;
 
@@ -30,3 +99,4 @@ describe('Test my sum function',function(){
     done()
   });
 })
+*/
