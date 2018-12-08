03d40a45999849cd1289cee68c305d2eecc40029
Cristiam Da Silva <cristiam86@gmail.com>
2018-12-08 13:53:22 +0100

    added winner player to rockPapperScissors function; tests also fixed

--- stuff/week5/webpack_test/test/myFirstTest.spec.js
+++ stuff/week5/webpack_test/test/myFirstTest.spec.js
@@ -1,22 +1,25 @@
-import {rockPaperScissor} from '../src/index.js';
+import {rockPaperScissors} from '../src/index.js';
 
 describe('Test hand1 wins', function() {
 
   test('scissors wins over paper', function(done) {
     var result = rockPaperScissors("scissors", "paper");
-    expect(result).toBe("scissors vs paper => scissors wins!")+
+    expect(result.msg).toBe("scissors vs paper => scissors wins!");
+    expect(result.winner).toBe("hand1");
     done();
   })
 
   test('rock wins over scissors', function(done) {
     var result = rockPaperScissors("rock", "scissors");
-    expect(result).toBe("rock vs scissors => rock wins!");
+    expect(result.msg).toBe("rock vs scissors => rock wins!");
+    expect(result.winner).toBe("hand1");
     done();
   })
 
   test('paper wins over rock', function(done) {
     var result = rockPaperScissors("paper", "rock");
-    expect(result).toBe("paper vs rock => paper wins!");
+    expect(result.msg).toBe("paper vs rock => paper wins!");
+    expect(result.winner).toBe("hand1");
     done();
   })
   
@@ -26,19 +29,22 @@ describe('Test hand2 wins', function() {
 
   test('scissors wins over paper', function(done) {
     var result = rockPaperScissors("paper", "scissors");
-    expect(result).toBe("paper vs scissors => scissors wins!")+
+    expect(result.msg).toBe("paper vs scissors => scissors wins!")+
+    expect(result.winner).toBe("hand2");
     done();
   })
 
   test('rock wins over scissors', function(done) {
     var result = rockPaperScissors("scissors", "rock");
-    expect(result).toBe("scissors vs rock => rock wins!");
+    expect(result.msg).toBe("scissors vs rock => rock wins!");
+    expect(result.winner).toBe("hand2");
     done();
   })
 
   test('paper wins over rock', function(done) {
     var result = rockPaperScissors("rock", "paper");
-    expect(result).toBe("rock vs paper => paper wins!");
+    expect(result.msg).toBe("rock vs paper => paper wins!");
+    expect(result.winner).toBe("hand2");
     done();
   })
   
@@ -48,19 +54,22 @@ describe('Test tie', function() {
 
   test('scissors tie', function(done) {
     var result = rockPaperScissors("scissors", "scissors");
-    expect(result).toBe("scissors vs scissors => tie!")+
+    expect(result.msg).toBe("scissors vs scissors => tie!")+
+    expect(result.winner).toBe("tie");
     done();
   })
 
   test('rock tie', function(done) {
     var result = rockPaperScissors("rock", "rock");
-    expect(result).toBe("rock vs rock => tie!");
+    expect(result.msg).toBe("rock vs rock => tie!");
+    expect(result.winner).toBe("tie");
     done();
   })
 
   test('paper tie', function(done) {
     var result = rockPaperScissors("paper", "paper");
-    expect(result).toBe("paper vs paper => tie!");
+    expect(result.msg).toBe("paper vs paper => tie!");
+    expect(result.winner).toBe("tie");
     done();
   })
   
