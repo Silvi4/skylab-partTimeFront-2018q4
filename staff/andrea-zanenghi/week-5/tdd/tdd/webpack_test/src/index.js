a1c37d42e0129cc8ad92fccc495964eb745288ae
andeeplus <andrea.zanenghi@gmail.com>
2018-12-11 13:52:39 +0100

    Fix
    fixed issue with button disable for autoplay when human testing is active

--- staff/andrea-zanenghi/week-5/tdd/tdd/webpack_test/src/index.js
+++ staff/andrea-zanenghi/week-5/tdd/tdd/webpack_test/src/index.js
@@ -182,6 +182,7 @@ function playHumans(){
 
       botActive = false;
       botVsBot = false;
+      $(".bot-bot").attr('disabled','disabled');
       $('.hand-2').children("img").remove();
       $('.img-load-1').children("img").remove();
       $('.img-load-2').children("img").remove();
@@ -196,6 +197,7 @@ function playHumans(){
     } else if(!$('#human').is(":checked")) {
       botActive = true;
       botVsBot = false;
+      $(".bot-bot").removeAttr('disabled');
       $('.img-load-1').children("img").remove();
       $('.img-load-2').children("img").remove();
       $('.hand-2 .rock-2').remove()
